7d20860445443a5939dab05e18ac09539f6ed859
Prashanth Batchu <prashanth@batchu.net>
2022-10-30 11:39:40 -0400

    Initial commit

---
+++ src/main/kotlin/app/zumy/edu/cricos_data_imorter/ZumyEduCricosDataImorterApplication.kt
@@ -0,0 +1,11 @@
+package app.zumy.edu.cricos_data_imorter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ZumyEduCricosDataImorterApplication
+
+fun main(args: Array<String>) {
+    runApplication<ZumyEduCricosDataImorterApplication>(*args)
+}
---
+++ src/test/kotlin/app/zumy/edu/cricos_data_imorter/ZumyEduCricosDataImorterApplicationTests.kt
@@ -0,0 +1,13 @@
+package app.zumy.edu.cricos_data_imorter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ZumyEduCricosDataImorterApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
